c1e37df681549ff70b23513c87c67c5d41bbd342
Richard Oven <71844377+rvnlive@users.noreply.github.com>
2021-09-03 17:36:52 +0100

    Front-end setup
    Front-end uses BootstrapVue, Vuex and Vue-router

---
+++ frontend/src/main.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import './plugins/bootstrap-vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+/* Importing plugins */
+import { IconsPlugin } from 'bootstrap-vue'
+
+Vue.use(IconsPlugin)
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: (h) => h(App)
+}).$mount('#app')
